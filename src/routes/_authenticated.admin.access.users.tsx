2ffab147856ac28ae9e5ec1c86c63c3be7d3640a
Nitin Rungta <157402466+nitinrungta1@users.noreply.github.com>
2026-05-02 02:46:34 +0530

    Delete src/routes/_authenticated.admin.access.users.tsx

--- src/routes/_authenticated.admin.access.users.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_authenticated/admin/access/users")({
-  beforeLoad: () => {
-    throw redirect({ to: "/admin/users" });
-  },
-});
